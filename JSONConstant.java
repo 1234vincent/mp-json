a065356ab6a2cdc6de4e7f46b15555d16331390f
rommin <nickyadl13@gmail.com>
2024-05-01 15:17:09 -0500

    Jsonhash.java is updated along with kvpair

--- JSONConstant.java
+++ JSONConstant.java
@@ -1,99 +1,74 @@
 import java.io.PrintWriter;
 
 /**
- * JSON constants.
+ * Represents immutable JSON constants.
  */
 public class JSONConstant implements JSONValue {
 
-  // +---------------+-----------------------------------------------
-  // | Static fields |
-  // +---------------+
+    // +---------------+-----------------------------------------------
+    // | Static fields |
+    // +---------------+
 
-  /**
-   * The true value.
-   */
-  public static final JSONConstant TRUE = new JSONConstant(Boolean.TRUE);
+    public static final JSONConstant TRUE = new JSONConstant(true);
+    public static final JSONConstant FALSE = new JSONConstant(false);
+    public static final JSONConstant NULL = new JSONConstant(null);
 
-  /**
-   * The false value.
-   */
-  public static final JSONConstant FALSE = new JSONConstant(Boolean.FALSE);
+    // +--------+------------------------------------------------------
+    // | Fields |
+    // +--------+
 
-  /**
-   * The null value.
-   */
-  public static final JSONConstant NULL = new JSONConstant(null);
+    private final Object value;
 
-  // +--------+------------------------------------------------------
-  // | Fields |
-  // +--------+
+    // +--------------+------------------------------------------------
+    // | Constructors |
+    // +--------------+
 
-  /**
-   * The value of the constant.
-   */
-  Object value;
+    /**
+     * Constructs a new constant.
+     * @param value The value to hold, which should be either a Boolean or null.
+     */
+    private JSONConstant(Object value) {
+        this.value = value;
+    }
 
-  // +--------------+------------------------------------------------
-  // | Constructors |
-  // +--------------+
+    // +-------------------------+-------------------------------------
+    // | Standard object methods |
+    // +-------------------------+
 
-  /**
-   * Build a new constant.
-   */
-  private JSONConstant(Object value) {
-    this.value = value;
-  } // JSONConstant
+    @Override
+    public String toString() {
+        if (value == null) {
+            return "null";
+        } else {
+            return value.toString();
+        }
+    }
 
-  // +-------------------------+-------------------------------------
-  // | Standard object methods |
-  // +-------------------------+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (!(other instanceof JSONConstant)) return false;
+        JSONConstant that = (JSONConstant) other;
+        return this.value == that.value; // Works because Boolean instances are cached by the JVM
+    }
 
-  /**
-   * Convert to a string (e.g., for printing).
-   */
-  public String toString() {
-    if (value == null) {
-      return "null";
-    } else {
-      return this.value.toString();
-    } // else
-  } // toString()
+    @Override
+    public int hashCode() {
+        return (value != null) ? value.hashCode() : 0;
+    }
 
-  /**
-   * Compare to another object.
-   */
-  public boolean equals(Object other) {
-    return ( ( (other instanceof JSONConstant) 
-               && (this.value == ((JSONConstant) other).value) )
-             || (this.value == other) );
-  } // equals(Object)
+    // +--------------------+------------------------------------------
+    // | Additional methods |
+    // +--------------------+
 
-  /**
-   * Compute the hash code.
-   */
-  public int hashCode() {
-    if (this.value == null)
-      return 0;
-    else
-      return this.value.hashCode();
-  } // hashCode()
+    @Override
+    public void writeJSON(PrintWriter pen) {
+        pen.print(this.toString());
+    }
 
-  // +--------------------+------------------------------------------
-  // | Additional methods |
-  // +--------------------+
-
-  /**
-   * Write the value as JSON.
-   */
-  public void writeJSON(PrintWriter pen) {
-    pen.print(this.toString());
-  } // writeJSON(PrintWriter)
-
-  /**
-   * Get the underlying value.
-   */
-  public Object getValue() {
-    return this.value;
-  } // getValue()
+    @Override
+    public Object getValue() {
+        return this.value;
+    }
 
 } // class JSONConstant

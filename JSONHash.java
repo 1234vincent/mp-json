521436ecc1288105c81797f2d1eb0bb42ddab6c7
rommin <nickyadl13@gmail.com>
2024-05-01 15:03:16 -0500

    Jsonhash.java is updated

--- JSONHash.java
+++ JSONHash.java
@@ -1,4 +1,5 @@
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
@@ -6,87 +7,155 @@ import java.util.Iterator;
  */
 public class JSONHash implements JSONValue {
 
-  // +--------+------------------------------------------------------
-  // | Fields |
-  // +--------+
+    // +--------+------------------------------------------------------
+    // | Fields |
+    // +--------+
 
-  // +--------------+------------------------------------------------
-  // | Constructors |
-  // +--------------+
+    private ArrayList<KVPair<JSONString, JSONValue>>[] table;
+    private int size;  // Number of key-value pairs in the hash
 
-  // +-------------------------+-------------------------------------
-  // | Standard object methods |
-  // +-------------------------+
+    // +--------------+------------------------------------------------
+    // | Constructors |
+    // +--------------+
 
-  /**
-   * Convert to a string (e.g., for printing).
-   */
-  public String toString() {
-    return "";          // STUB
-  } // toString()
+    @SuppressWarnings("unchecked")
+    public JSONHash() {
+        table = (ArrayList<KVPair<JSONString, JSONValue>>[]) new ArrayList[10];  // initial capacity
+        for (int i = 0; i < table.length; i++) {
+            table[i] = new ArrayList<>();
+        }
+        size = 0;
+    }
 
-  /**
-   * Compare to another object.
-   */
-  public boolean equals(Object other) {
-    return true;        // STUB
-  } // equals(Object)
+    // +-------------------------+-------------------------------------
+    // | Standard object methods |
+    // +-------------------------+
 
-  /**
-   * Compute the hash code.
-   */
-  public int hashCode() {
-    return 0;           // STUB
-  } // hashCode()
+    /**
+     * Convert to a string (e.g., for printing).
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{");
+        Iterator<KVPair<JSONString, JSONValue>> it = this.iterator();
+        while (it.hasNext()) {
+            KVPair<JSONString, JSONValue> pair = it.next();
+            sb.append(pair.toString());
+            if (it.hasNext()) {
+                sb.append(", ");
+            }
+        }
+        sb.append("}");
+        return sb.toString();
+    } // toString()
 
-  // +--------------------+------------------------------------------
-  // | Additional methods |
-  // +--------------------+
+    /**
+     * Compare to another object.
+     */
+    public boolean equals(Object other) {
+        if (!(other instanceof JSONHash)) return false;
+        JSONHash that = (JSONHash) other;
+        if (this.size() != that.size()) return false;
+        Iterator<KVPair<JSONString, JSONValue>> it = this.iterator();
+        while (it.hasNext()) {
+            KVPair<JSONString, JSONValue> pair = it.next();
+            JSONValue thatValue = that.get(pair.key());
+            if (!pair.value().equals(thatValue)) {
+                return false;
+            }
+        }
+        return true;
+    } // equals(Object)
 
-  /**
-   * Write the value as JSON.
-   */
-  public void writeJSON(PrintWriter pen) {
-                        // STUB
-  } // writeJSON(PrintWriter)
+    /**
+     * Compute the hash code.
+     */
+    public int hashCode() {
+        int hash = 0;
+        for (KVPair<JSONString, JSONValue> pair : this) {
+            hash += pair.hashCode();
+        }
+        return hash;
+    } // hashCode()
 
-  /**
-   * Get the underlying value.
-   */
-  public Iterator<KVPair<JSONString,JSONValue>> getValue() {
-    return this.iterator();
-  } // getValue()
+    // +--------------------+------------------------------------------
+    // | Additional methods |
+    // +--------------------+
 
-  // +-------------------+-------------------------------------------
-  // | Hashtable methods |
-  // +-------------------+
+    /**
+     * Write the value as JSON.
+     */
+    public void writeJSON(PrintWriter pen) {
+        pen.print(this.toString());
+    } // writeJSON(PrintWriter)
 
-  /**
-   * Get the value associated with a key.
-   */
-  public JSONValue get(JSONString key) {
-    return null;        // STUB
-  } // get(JSONString)
+    /**
+     * Get the underlying value.
+     */
+    public Iterator<KVPair<JSONString, JSONValue>> getValue() {
+        return this.iterator();
+    } // getValue()
 
-  /**
-   * Get all of the key/value pairs.
-   */
-  public Iterator<KVPair<JSONString,JSONValue>> iterator() {
-    return null;        // STUB
-  } // iterator()
+    // +-------------------+-------------------------------------------
+    // | Hashtable methods |
+    // +-------------------+
 
-  /**
-   * Set the value associated with a key.
-   */
-  public void set(JSONString key, JSONValue value) {
-                        // STUB
-  } // set(JSONString, JSONValue)
+    /**
+     * Get the value associated with a key.
+     */
+    public JSONValue get(JSONString key) {
+        int index = hash(key);
+        for (KVPair<JSONString, JSONValue> pair : table[index]) {
+            if (pair.key().equals(key)) {
+                return pair.value();
+            }
+        }
+        return null;
+    } // get(JSONString)
 
-  /**
-   * Find out how many key/value pairs are in the hash table.
-   */
-  public int size() {
-    return 0;           // STUB
-  } // size()
+    /**
+     * Set the value associated with a key, replacing the existing KVPair if the key exists.
+     */
+    public void set(JSONString key, JSONValue value) {
+        int index = hash(key);
+        // Iterate over the bucket to find an existing key
+        for (int i = 0; i < table[index].size(); i++) {
+            if (table[index].get(i).key().equals(key)) {
+                table[index].set(i, new KVPair<>(key, value));  // Replace with new KVPair
+                return;
+            }
+        }
+        // Key not found, add new KVPair
+        table[index].add(new KVPair<>(key, value));
+        size++;
+    } // set(JSONString, JSONValue)
+
+    /**
+     * Get all of the key/value pairs.
+     */
+    public Iterator<KVPair<JSONString, JSONValue>> iterator() {
+        ArrayList<KVPair<JSONString, JSONValue>> result = new ArrayList<>();
+        for (ArrayList<KVPair<JSONString, JSONValue>> bucket : table) {
+            result.addAll(bucket);
+        }
+        return result.iterator();
+    } // iterator()
+
+    /**
+     * Find out how many key/value pairs are in the hash table.
+     */
+    public int size() {
+        return size;
+    } // size()
+
+    // +------------------+--------------------------------------------
+    // | Helper methods |
+    // +------------------+
+
+    /**
+     * Compute the index in the table for a given key.
+     */
+    private int hash(JSONString key) {
+        return (key.hashCode() & 0x7fffffff) % table.length;
+    } // hash(JSONString)
 
 } // class JSONHash
